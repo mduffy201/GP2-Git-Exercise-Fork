027d34dd97b00f04b2f2f1755cab78a4530c6d4c
Brian McDonald <brian.mcdonald@gcu.ac.uk>
2013-09-22 15:56:24 +0100

    Added new line and project

---
+++ HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout<<"Hello World, press enter key to exit"<<endl;
+	cout<<"I am a code monkey"<<endl;
+	cin.ignore();
+	
+	return 0;
+}
